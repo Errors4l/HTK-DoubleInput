bc5f858206be657b00c5c58dcd38e4db47bf1571
Errors4l <Errors4l@hotmail.nl>
2017-06-01 09:37:57 +0200

    Add project
    Includes HoloToolkit v1.5.7.0. Please refer to the license in the HoloToolkit asset folder.

---
+++ Assets/RotationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+public class RotationManager : Singleton<RotationManager>
+{
+    public GameObject TargetObject;
+
+    public void RotateClockwise(float degrees)
+    {
+        if (TargetObject == null)
+        {
+            Debug.LogError("RotationManager has no TargetObject set!");
+            return;
+        }
+
+        TargetObject.transform.Rotate(Vector3.up, degrees);
+        Debug.LogFormat("TargetObject rotated {0} degrees.", degrees);
+    }
+}
